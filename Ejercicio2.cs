411474551c7be89f62243413dede1a612a225168
Adonay Valencia <adonayvm32@gmail.com>
2019-09-04 12:14:16 -0600

    Ejercicio 1 casi terminados

---
+++ Ejercicio2.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Práctica6
+{
+    class Ejercicio2
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(Dibujo(5,5));
+            Console.ReadKey();
+        }
+
+        static string Dibujo(int ancho, int alto)
+        {
+            string figura = "";
+
+            for (int i = 1; i <= alto; i++)
+            {
+                figura = figura + "\n";
+                for (int i2 = 1; i2 <= ancho; i2++)
+                {
+                    if (i == 1 || i == alto)
+                    {
+                        figura = figura + "* ";
+                    }
+                    else
+                    {
+                        if (i2 == 1 || i2 == ancho)
+                        {
+                            figura = figura + "* ";
+                        }
+                        else
+                        {
+                            figura = figura + "  ";
+                        }
+
+                    }
+
+                }
+            }
+
+            return figura;
+        }
+
+        
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Práctica6
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+        }
+    }
+}
