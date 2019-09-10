ca35d52b0aa4860358f2379689f06309f3e73e93
Adonay Valencia <adonayvm32@gmail.com>
2019-09-10 17:30:25 -0600

    Rama 1

--- Ejercicio2.cs
+++ Ejercicio2.cs
@@ -6,13 +6,104 @@ namespace Práctica6
 {
     class Ejercicio2
     {
+        static int Alto = 0, Ancho = 0, x = 0, y = 0;
+        static string caracter;
         static void Main(string[] args)
         {
-            Console.WriteLine(Dibujo(5,5));
+            Console.Write("Alto: ");
+            Alto = Convert.ToInt32(Console.ReadLine());
+            Console.Write("\nAncho: ");
+            Ancho = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("\n/////////////////////////////////////////////////////////");
+            Console.WriteLine("\nNOTA: DEJAR EN BLANCO PARA OCUPAR DATOS PREDETERMINADOS");
+            Console.WriteLine("\nCaracter a utilizar: ");
+            caracter = Console.ReadLine();
+            if (caracter == "")
+            {
+
+                coor();
+                if (x == 0 && y == 0)
+                {
+                    Console.Clear();
+                    Dibujo(Ancho, Alto);
+                }
+                else
+                {
+                    Console.Clear();
+                    caracter = "*";
+                    Dibujo(Ancho, Alto, caracter, x, y);
+                }
+
+            }
+            else
+            {
+                coor();
+                if (x == 0 && y == 0)
+                {
+                    Console.Clear();
+                    Dibujo(Ancho, Alto, caracter);
+                }
+                else
+                {
+                    Console.Clear();
+                    Dibujo(Ancho, Alto, caracter, x, y);
+
+                }
+            }
             Console.ReadKey();
         }
 
-        static string Dibujo(int ancho, int alto)
+        static void coor()
+        {
+            Console.WriteLine("\nCoordenandas:" +
+                    "\nX: ");
+            if (int.TryParse(Console.ReadLine(), out x))
+            {
+                coX:
+                Console.WriteLine("\nY: ");
+                if (int.TryParse(Console.ReadLine(), out y))
+                {
+                    Console.Clear();
+                    Dibujo(Ancho, Alto);
+                }
+                else
+                {
+                    Console.Clear();
+                    goto coX;
+                }
+            }
+        }
+        static void Dibujo(int ancho, int alto)
+        {
+            string figura = "";
+            string carac = "* ";
+
+            for (int i = 1; i <= alto; i++)
+            {
+                figura = figura + "\n";
+                for (int i2 = 1; i2 <= ancho; i2++)
+                {
+                    if (i == 1 || i == alto)
+                    {
+                        figura = figura + carac;
+                    }
+                    else
+                    {
+                        if (i2 == 1 || i2 == ancho)
+                        {
+                            figura = figura + carac;
+                        }
+                        else
+                        {
+                            figura = figura + "  ";
+                        }
+                    }
+                }
+            }
+            Console.WriteLine(figura);
+        }
+
+        static void Dibujo(int ancho, int alto, string cara)
         {
             string figura = "";
 
@@ -23,27 +114,52 @@ namespace Práctica6
                 {
                     if (i == 1 || i == alto)
                     {
-                        figura = figura + "* ";
+                        figura = figura + cara + " ";
                     }
                     else
                     {
                         if (i2 == 1 || i2 == ancho)
                         {
-                            figura = figura + "* ";
+                            figura = figura + cara + " ";
                         }
                         else
                         {
                             figura = figura + "  ";
                         }
-
                     }
-
                 }
             }
-
-            return figura;
+            Console.WriteLine(figura);
         }
 
-        
+        static void Dibujo(int ancho, int alto, string cara, int X, int Y)
+        {
+            Console.SetCursorPosition(X, Y);
+            string figura = "";
+            for (int i = 1; i <= alto; i++)
+            {
+                figura = figura + "\n";
+                for (int i2 = 1; i2 <= ancho; i2++)
+                {
+                    if (i == 1 || i == alto)
+                    {
+                        figura = figura + cara + " ";
+                    }
+                    else
+                    {
+                        if (i2 == 1 || i2 == ancho)
+                        {
+                            figura = figura + cara + " ";
+                        }
+                        else
+                        {
+                            figura = figura + "  ";
+
+                        }
+                    }
+                }
+            }
+            Console.WriteLine(figura);
+        }
     }
 }
--- Program.cs
+++ Program.cs
@@ -4,9 +4,84 @@ namespace Práctica6
 {
     class Program
     {
+        //Valencia Machado, Elí Adonay
+        //08-09-19
         static void Main(string[] args)
         {
-            
+            int nump, colorp = 5;
+            Console.WriteLine("Ingrese un número: ");
+            nump = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("\n/////////////////////////////////////////////////////////");
+            Console.WriteLine("\nNOTA: DEJAR EN BLANCO PARA OCUPAR DATOS PREDETERMINADOS");
+            try
+            {
+                Console.WriteLine("\nCaolor a utilizar: ");
+                Console.WriteLine("\n [1] Azul" +
+                                  "\n [2] Rojo" +
+                                  "\n [3] Verde" +
+                                  "\n [4] Amarillo");
+                colorp = Convert.ToInt32(Console.ReadLine());
+            }
+            catch (FormatException)
+            {
+                Console.WriteLine("Color de predeterminado.");
+            }
+            goto SALIDA;
+
+                SALIDA: 
+
+            Console.Clear();
+            Console.WriteLine("Tabla del " + nump + ":\n" + tabla(nump));
+            Console.ReadKey();
+        }
+
+        static string tabla(int num)
+        {
+            string multi = "";
+            for (int i = 1; i <= 10; i++)
+            {
+                multi = multi + "\n" + Convert.ToString(num);
+                multi = multi + " * " + Convert.ToString(i);
+                multi = multi + " = " + Convert.ToString(num * i);
+
+
+            }
+            return multi;
+        }
+
+        static string tabla(int num, int color = 5)
+        {
+
+            switch (color)
+            {
+                case 1:
+                    Console.ForegroundColor = ConsoleColor.Blue;
+                    Console.WriteLine("Azul");
+                    break;
+                case 2:
+                    Console.ForegroundColor = ConsoleColor.Red;
+                    Console.WriteLine("Rojo");
+                    break;
+                case 3:
+                    Console.ForegroundColor = ConsoleColor.Green;
+                    Console.WriteLine("Verde");
+                    break;
+                case 4:
+                    Console.ForegroundColor = ConsoleColor.Yellow;
+                    Console.WriteLine("Amarillo");
+                    break;
+                default:
+                    Console.WriteLine("Color por defecto\n");
+                    break;
+            }
+            string multi = "";
+            for (int i = 1; i <= 10; i++)
+            {
+                multi = multi + "\n" + Convert.ToString(num);
+                multi = multi + " * " + Convert.ToString(i);
+                multi = multi + " = " + Convert.ToString(num * i);
+            }
+            return multi;
         }
     }
 }
